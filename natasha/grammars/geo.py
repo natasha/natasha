f2000341598656f6d0fae16117398bf1585cea7e
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-17 00:16:03 +0300

    Add PartialObject rule to geo grammar, add new words to ComplexObject prefix dictionary

--- natasha/grammars/geo.py
+++ natasha/grammars/geo.py
@@ -27,9 +27,14 @@ class Geo(Enum):
     ComplexObject = (
         ('word', {
             'labels': [
+                ('gram', 'ADJF'),
                 ('dictionary', {
                     'северный',
+                    'северо-западный',
+                    'северо-восточный',
                     'южный',
+                    'юго-западный',
+                    'юго-восточный',
                     'западный',
                     'восточный',
                     'верхний',
@@ -44,6 +49,43 @@ class Geo(Enum):
                 ('gram', 'NOUN'),
                 ('gram', 'Geox'),
                 ('gender-match', -1),
+            ],
+        }),
+        TERM,
+    )
+
+    PartialObject = (
+        ('word', {
+            'labels': [
+                ('gram', 'NOUN'),
+                ('dictionary', {
+                    'север',
+                    'северо-восток',
+                    'северо-запад',
+                    'юг',
+                    'юго-восток',
+                    'юго-запад',
+                    'запад',
+                    'восток',
+                })
+            ],
+        }),
+        ('word', {
+            'labels': [
+                ('gram', 'NOUN'),
+                ('gram', 'Geox'),
+                ('gender-match', -1),
+            ],
+        }),
+        TERM,
+    )
+
+    Object = (
+        ('word', {
+            'labels': [
+                ('is-capitalized', None),
+                ('gram', 'Geox'),
+                ('gram-not', 'Abbr'),
             ],
         }),
         TERM,
