5f11365d120220d3ef189528f06e90458ebba0b2
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-19 00:53:54 +0300

    Use yargy.combinator

--- natasha/__init__.py
+++ natasha/__init__.py
@@ -1,29 +1,15 @@
 from copy import copy
 from collections import deque
 
-from yargy import FactParser
-
+from yargy import FactParser, Combinator
 from natasha.grammars import Person, Geo, Money, Date, Brand, Event
 
 
-class Combinator(object):
-
-    DEFAULT_GRAMMARS = [
-        Money,
-        Person,
-        Geo,
-        Date,
-        Brand,
-        Event,
-    ]
-
-    def __init__(self, grammars=None, cache_size=50000):
-        self.grammars = grammars or self.DEFAULT_GRAMMARS
-        self.parser = FactParser(cache_size=cache_size)
-
-    def extract(self, text):
-        tokens = deque(self.parser.tokenizer.transform(text))
-        for grammar in self.grammars:
-            for grammar_type, rule in grammar.__members__.items():
-                for match in self.parser.extract(copy(tokens), rule.value):
-                    yield (grammar, grammar_type, match)
+DEFAULT_GRAMMARS = [
+    Money,
+    Person,
+    Geo,
+    Date,
+    Brand,
+    Event,
+]
