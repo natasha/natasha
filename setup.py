60e9698737a51cec33176c2e1ab94852456ed0df
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-18 07:06:13 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='natasha',
+    version='0.1.0',
+    description='Named-entity recognition for russian language',
+    url='https://github.com/bureaucratic-labs/natasha',
+    author='Dmitry Veselov',
+    author_email='d.a.veselov@yandex.ru',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='natural language processing russian morphology named entity recognition tomita',
+    packages=find_packages(),
+    install_requires=[
+        'yargy'
+    ],
+)
