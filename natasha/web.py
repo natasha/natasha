c6f1b230de887a5f7b855157e5f5587aa74a226e
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-08-20 00:08:53 +0300

    Make partial ujson.dumps function with ensure_ascii=False

--- natasha/web.py
+++ natasha/web.py
@@ -1,6 +1,12 @@
 from ujson import dumps
 from aiohttp import web
+from functools import partial
+
+
 from natasha import Combinator, DEFAULT_GRAMMARS
+
+
+json_dumps = partial(dumps, ensure_ascii=False)
 
 
 def serialize(results):
@@ -14,7 +20,7 @@ def serialize(results):
 async def index(request):
     form = await request.post()
     results = app['combinator'].extract(form['text'])
-    return web.json_response(list(serialize(results)), dumps=dumps)
+    return web.json_response(list(serialize(results)), dumps=json_dumps)
 
 
 app = web.Application()
