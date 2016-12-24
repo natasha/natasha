d13eb68ff448e317080fb18409856980ca54ba75
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-12-25 01:26:35 +0300

    Implemented special command that build pipeline dictionaries at setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
-from setuptools import setup, find_packages
+from setuptools import (
+    Command,
+    setup,
+    find_packages,
+)
+from natasha import (
+    __version__,
+    BUILD_DEFAULT_PIPELINES,
+)
+
+
+class BuildDictionariesCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        self.pipelines = BUILD_DEFAULT_PIPELINES()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print('=> Building pipeline dictionaries ...')
+        for pipeline in self.pipelines:
+            print('Building', pipeline.__class__.__name__, '...')
+            pipeline.build()
+
 
 setup(
     name='natasha',
-    version='0.4.0',
+    version=__version__,
     description='Named-entity recognition for russian language',
     url='https://github.com/bureaucratic-labs/natasha',
     author='Dmitry Veselov',
@@ -11,12 +37,29 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
     ],
+    cmdclass={
+        'build_dicts': BuildDictionariesCommand,
+    },
+    package_data = {
+        'natasha.grammars': [
+            'dictionaries/*.dawg',
+        ],
+    },
     keywords='natural language processing, russian morphology, named entity recognition, tomita',
     packages=find_packages(),
     install_requires=[
