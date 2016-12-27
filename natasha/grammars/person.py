6c34ffb68450cfd84580657c93a0225e79acf7ea
Dmitry Veselov <d.a.veselov@yandex.ru>
2016-12-27 07:49:36 +0300

    Implemented ProbabilisticPerson grammars class (which shows good result on factRuEval-2016 dataset, but not included in DEFAULT_GRAMMARS) and additional rules (partial initials plus surname) for Person grammar

--- natasha/grammars/person.py
+++ natasha/grammars/person.py
@@ -10,15 +10,19 @@ from yargy.labels import (
     gnc_match,
     is_capitalized,
     dictionary,
+    is_upper,
+    gram_any,
 )
 
-NAME_PARTICLE_DICTIONARY = {
+NAME_NOBILITY_PARTICLE_DICTIONARY = {
     'да',
     'де',
     'ди',
     'ван',
     'фон',
     'дель',
+    'бен',
+    'дю',
 }
 
 class Person(Enum):
@@ -108,6 +112,16 @@ class Person(Enum):
         InitialsAndLastname[-1],
     ] + InitialsAndLastname[:4]
 
+    # Раневская Л.
+    LastnameAndFirstnameAsInitials = [
+        InitialsAndLastname[-1],
+    ] + InitialsAndLastname[:2]
+
+    # Л. Раневская
+    FistnameAsInitialsAndLastname = InitialsAndLastname[:2] + [
+        InitialsAndLastname[-1],
+    ]
+
     # Иван Иванов
     FisrtnameAndLastname = [
         {
@@ -193,11 +207,11 @@ class Person(Enum):
     ]
 
     # Отто фон Бисмарк
-    FirstnameAndLastnameWithParticle = [
+    FirstnameAndLastnameWithNobilityParticle = [
         FullReversed[0],
         {
             'labels': [
-                dictionary(NAME_PARTICLE_DICTIONARY),
+                dictionary(NAME_NOBILITY_PARTICLE_DICTIONARY),
             ],
         },
         {
@@ -207,4 +221,71 @@ class Person(Enum):
                 gnc_match(0, solve_disambiguation=True)
             ],
         },
+    ]
+
+
+POSSIBLE_PART_OF_NAME_GRAMMAR = {
+    'labels': [
+        gram('NOUN'),
+        gram_any({'masc', 'femn'}),
+        gram_not('inan'),
+        gram_not('Abbr'),
+        is_upper(False),
+        is_capitalized(True),
+    ]
+}
+
+POSSIBLE_PART_OF_NAME_WITH_GNC_MATCH_GRAMMAR = {
+    'labels': [
+        gram('NOUN'),
+        gram_any({'masc', 'femn'}),
+        gram_not('Abbr'),
+        is_capitalized(True),
+        gnc_match(-1, solve_disambiguation=True),
+    ]
+}
+
+
+class ProbabilisticPerson(Enum):
+
+    '''
+    This grammars matches words that looks like (but may not to be) person names
+    Not included in natasha DEFAULT_GRAMMARS, but shows good result on factRuEval-16 testset
+    '''
+
+    Full = [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+        POSSIBLE_PART_OF_NAME_WITH_GNC_MATCH_GRAMMAR,
+        POSSIBLE_PART_OF_NAME_WITH_GNC_MATCH_GRAMMAR,
+    ]
+
+    FirstnameAndLastname = [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+        POSSIBLE_PART_OF_NAME_WITH_GNC_MATCH_GRAMMAR,
+    ]
+
+    InitialsAndLastname = Person.InitialsAndLastname.value[:4] + [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+    ]
+
+    LastnameAndInitials = [
+        POSSIBLE_PART_OF_NAME_GRAMMAR
+    ] + Person.InitialsAndLastname.value[:4]
+
+    FirstnameAsInitialsAndLastname = Person.InitialsAndLastname.value[:2] + [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+    ]
+
+    LastnameAndfirstnameAsInitials = [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+    ] + Person.InitialsAndLastname.value[:2]
+
+    FirstnameAndLastnameWithNobilityParticle = [
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
+        {
+            'labels': [
+                dictionary(NAME_NOBILITY_PARTICLE_DICTIONARY),
+            ],
+        },
+        POSSIBLE_PART_OF_NAME_GRAMMAR,
     ]
