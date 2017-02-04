a23adc96e307f632554b4269fc27b19100eb6b8c
Dmitry Veselov <d.a.veselov@yandex.ru>
2017-02-04 09:07:53 +0300

    Import probabilistic models at natasha.grammars package

--- natasha/grammars/__init__.py
+++ natasha/grammars/__init__.py
@@ -1,7 +1,7 @@
-from natasha.grammars.person import Person
+from natasha.grammars.person import Person, ProbabilisticPerson
 from natasha.grammars.location import Location, Address
 from natasha.grammars.money import Money
 from natasha.grammars.date import Date
 from natasha.grammars.brand import Brand
 from natasha.grammars.event import Event
-from natasha.grammars.organisation import Organisation
+from natasha.grammars.organisation import Organisation, ProbabilisticOrganisation
