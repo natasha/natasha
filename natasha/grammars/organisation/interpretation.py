d9ead6dc819ae86c784f0a626e1c25e4f63de712
Dmitry Veselov <d.a.veselov@yandex.ru>
2017-03-21 14:27:54 +0300

    Custom equality method for organisation objects

--- natasha/grammars/organisation/interpretation.py
+++ natasha/grammars/organisation/interpretation.py
@@ -34,3 +34,22 @@ class OrganisationObject(InterpretationObject):
             ).lower()
         else:
             return None
+
+    def normalized_name_difference(self, another):
+        return damerau_levenshtein_distance(
+            self.normalized_name,
+            another.normalized_name,
+        )
+
+    def __eq__(self, another):
+        if self.normalized_name and another.normalized_name:
+            a, b = self.normalized_name, another.normalized_name
+            if b > a:
+                a, b = b, a
+            if b in a:
+                return True
+            if self.abbr & another.abbr:
+                return True
+            if self.normalized_name_difference(another) <= self.SIMILARITY_THRESHOLD:
+                return True
+        return False
