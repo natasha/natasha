eacc1ef62bbe6daa208aa9e6c9fef6e25ebbf5cf
Kukushkin Alexander <alex@alexkuk.ru>
2017-04-01 15:21:43 +0300

    Replace string_required with type_required

--- natasha/grammars/organisation/grammars.py
+++ natasha/grammars/organisation/grammars.py
@@ -21,7 +21,8 @@ from yargy.labels import (
     or_,
     label,
     is_upper,
-    string_required,
+    type_required,
+    string_type
 )
 from yargy.parser import OR
 from yargy.normalization import NormalizationType
@@ -156,7 +157,7 @@ PROBABILISTIC_NAMED_ORG_INITIALS_RULE = [
 ]
 
 @label
-@string_required
+@type_required(string_type)
 def is_abbr(case, token, value):
     '''
     Returns true if token contains only uppercased letters
