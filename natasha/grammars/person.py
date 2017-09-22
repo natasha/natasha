a90b50f3897aa7e6caf57227afcb47e6bf22ff6c
Kukushkin Alexander <alex@alexkuk.ru>
2017-09-22 11:08:24 +0300

    Add where

--- natasha/grammars/person.py
+++ natasha/grammars/person.py
@@ -2,12 +2,32 @@
 from __future__ import unicode_literals
 
 from yargy import (
-    rule, fact
+    rule, fact,
+    or_
 )
 from yargy.predicates import gram
 from yargy.pipelines import MorphPipeline
 
-from .name import NAME
+from .name import (
+    Name,
+
+    FIRST_LAST,
+    LAST_FIRST,
+    TITLE_FIRST_LAST,
+    TITLE_LAST_FIRST,
+
+    ABBR_FIRST_LAST,
+    LAST_ABBR_FIRST,
+    ABBR_FIRST_MIDDLE_LAST,
+    LAST_ABBR_FIRST_MIDDLE,
+
+    TITLE_FIRST_MIDDLE,
+    TITLE_FIRST_MIDDLE_LAST,
+    TITLE_LAST_FIRST_MIDDLE,
+
+    JUST_FIRST,
+    JUST_LAST
+)
 
 
 Person = fact(
@@ -306,13 +326,58 @@ class PositionsPipeline(MorphPipeline):
 
 POSITION = gram('Position')
 
-PERSON = rule(
-    POSITION.optional().interpretation(
-        Person.position
+GENT = gram('gent')
+
+WHERE = or_(
+    rule(GENT),
+    rule(GENT, GENT),
+    rule(GENT, GENT, GENT),
+    rule(GENT, GENT, GENT, GENT),
+    rule(GENT, GENT, GENT, GENT, GENT),
+)
+
+POSITION = rule(
+    POSITION,
+    WHERE.optional()
+).interpretation(
+    Person.position
+)
+
+COMPLEX = or_(
+    FIRST_LAST,
+    LAST_FIRST,
+    TITLE_FIRST_LAST,
+    TITLE_LAST_FIRST,
+
+    ABBR_FIRST_LAST,
+    LAST_ABBR_FIRST,
+    ABBR_FIRST_MIDDLE_LAST,
+    LAST_ABBR_FIRST_MIDDLE,
+
+    TITLE_FIRST_MIDDLE,
+    TITLE_FIRST_MIDDLE_LAST,
+    TITLE_LAST_FIRST_MIDDLE,
+).interpretation(
+    Name
+).interpretation(
+    Person.name
+)
+
+SIMPLE = or_(
+    JUST_FIRST,
+    JUST_LAST
+).interpretation(
+    Name
+).interpretation(
+    Person.name
+)
+
+PERSON = or_(
+    rule(
+        POSITION.optional(),
+        COMPLEX
     ),
-    NAME.interpretation(
-        Person.name
-    )
+    SIMPLE
 ).interpretation(
     Person
 )
