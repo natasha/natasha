c27b65b74d15ff8534097fcc542cafa9d50b374f
Sergey Tihonov <eni.basic@gmail.com>
2017-12-04 10:47:07 +0500

    Advanced money (#40)
    * Добавил расширенную поддержку определения денег
    * Добавил тесты

--- natasha/grammars/money.py
+++ natasha/grammars/money.py
@@ -8,17 +8,20 @@ from yargy import (
 )
 from yargy.predicates import (
     eq, in_,
-    gram, normalized, caseless
-)
+    gram, normalized, caseless,
+    dictionary)
 
 
 Money = fact(
     'Money',
-    ['amount', 'currency']
+    ['amount', 'range', 'currency']
 )
 
 
-EURO = normalized('евро')
+EURO = or_(
+    normalized('евро'),
+    eq('€')
+)
 
 DOLLARS = or_(
     normalized('доллар'),
@@ -30,7 +33,8 @@ RUBLES = or_(
     rule(
         or_(
             caseless('руб'),
-            caseless('р')
+            caseless('р'),
+            eq('₽')
         ),
         eq('.').optional()
     )
@@ -46,29 +50,91 @@ CURRENCY = or_(
 
 INT = gram('INT')
 
+AMWORD = rule(
+    or_(
+        dictionary({
+            'тысяча',
+            'миллион'
+        }),
+        eq('т')
+    ),
+    eq('.').optional()
+)
+
+SEP = in_({',', '.'})
+
 AMOUNT_ = or_(
     rule(INT),
     rule(INT, INT),
     rule(INT, INT, INT),
-    rule(INT, '.', INT),
-    rule(INT, '.', INT, '.', INT),
+    rule(INT, SEP, INT),
+    rule(INT, SEP, INT, SEP, INT),
 )
 
 FRACTION_AMOUN = rule(
     AMOUNT_,
-    in_({',', '.'}),
+    SEP,
     INT
 )
 
 AMOUNT = or_(
     AMOUNT_,
-    FRACTION_AMOUN
+    rule(AMOUNT_, AMWORD),
+    FRACTION_AMOUN,
+    rule(FRACTION_AMOUN, AMWORD)
 ).interpretation(
     Money.amount
 )
 
+RANGE_OT = rule(
+    eq('от'),
+    AMOUNT
+)
+
+RANGE_DO = rule(
+    eq('до'),
+    AMOUNT
+)
+
+RANGE = or_(
+    RANGE_OT,
+    RANGE_DO,
+    rule(
+        RANGE_OT,
+        RANGE_DO
+    ),
+    rule(
+        AMOUNT_,
+        eq('-'),
+        AMOUNT_
+    )
+).interpretation(
+    Money.range
+)
+
+TIME = rule(
+    or_(
+        eq('/'),
+        eq('в')
+    ).optional(),
+    dictionary({
+        'сутки',
+        'час',
+        'смена'
+    })
+)
+
 MONEY = rule(
-    AMOUNT, CURRENCY
+    or_(
+        AMOUNT,
+        RANGE,
+        rule(RANGE, CURRENCY),
+        rule(RANGE, CURRENCY, TIME),
+        rule(RANGE, AMWORD, CURRENCY),
+        rule(RANGE, AMWORD, CURRENCY, TIME),
+        rule(AMOUNT, CURRENCY),
+        rule(AMOUNT, CURRENCY, TIME),
+    )
 ).interpretation(
     Money
 )
