c0b639951ecfa9e04798c6d7aca82a8373db5047
Alexandr N Zamaraev (aka tonal) <tonal@promsoft.ru>
2018-03-07 11:42:21 +0700

    Добавил тесты для посёлка городского типа

--- natasha/tests/test_address.py
+++ natasha/tests/test_address.py
@@ -71,7 +71,35 @@ tests = [
             Building(number='8 Б', type='дом'),
             Room(number='4', type='офис')
         ])
-    ]
+    ],
+    [
+        'Самарская область, п.г.т. Алексеевка, ул. Ульяновская д 21',
+        Address(parts=[
+            Region(name='Самарская', type='область'),
+            Settlement(name='Алексеевка', type='посёлок'),
+            Street(name='Ульяновская', type='улица'),
+            Building(number='21', type='дом')
+        ])
+
+    ],
+    [
+        'Белгородская обл, пгт Борисовка,ул. Рудого д160',
+        Address(parts=[
+            Region(name='Белгородская', type='область'),
+            Settlement(name='Борисовка', type='посёлок'),
+            Street(name='Рудого', type='улица'),
+            Building(number='160', type='дом')
+        ])
+    ],
+    [
+        'Мурманская обл поселок городского типа Молочный, ул.Гальченко д.11',
+        Address(parts=[
+            Region(name='Мурманская', type='область'),
+            Settlement(name='Молочный', type='посёлок'),
+            Street(name='Гальченко', type='улица'),
+            Building(number='11', type='дом')
+        ])
+    ],
 ]
 
 
