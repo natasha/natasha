25c4cc860700db22ed99909c23e20693cef9128d
Kukushkin Alexander <alex@alexkuk.ru>
2020-08-20 16:19:50 +0300

    Fix envelope spans

---
+++ natasha/tests/test_span.py
@@ -0,0 +1,55 @@
+
+import pytest
+
+from natasha.span import (
+    Span,
+    envelop_spans
+)
+
+
+tests = [
+    [
+        [(0, 1)],
+        [],
+        []
+    ],
+    [
+        [],
+        [(0, 1)],
+        [[]]
+    ],
+    [
+        [(0, 1), (1, 2)],
+        [(1, 2)],
+        [[(1, 2)]]
+    ],
+    [
+        [(0, 1), (1, 2)],
+        [(0, 1)],
+        [[(0, 1)]]
+    ],
+    [
+        [(0, 1), (1, 2)],
+        [(0, 1), (1, 2)],
+        [[(0, 1)], [(1, 2)]]
+    ],
+    
+]
+
+
+def adapt_spans(spans):
+    for start, stop in spans:
+        yield Span(start, stop, type=None)
+
+
+@pytest.mark.parametrize('test', tests)
+def test_envelope_spans(test):
+    spans, envelopes, target = test
+    spans = list(adapt_spans(spans))
+    envelopes = list(adapt_spans(envelopes))
+    target = [
+        list(adapt_spans(group))
+        for group in target
+    ]
+    pred = list(envelop_spans(spans, envelopes))
+    assert pred == target
